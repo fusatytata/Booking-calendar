91e8c2734ccafd5491c182028a8cc52b3894012c
fusatytata <simon.stripes@gmail.com>
2024-04-29 15:24:30 +0200

    main structure of the app added - the structure is managed by 3 components, first MainNavbar is just a dummy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from "react";
+
+import MainNavbar from "./MainNavbar";
+
+
+import "./App.css";
+
+import moment from "moment/moment";
+import DayTimes from "./DayTimes";
+import DateNavbar from "./DateNavbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [date, setDate] = useState(moment());
+    
+
+    return (
+        <div>
+            <MainNavbar />
+            <DateNavbar date={date} setDate={setDate}/>
+            <DayTimes date={date} />
+           
+
+            
+        </div>
+    );
 }
 
 export default App;
+
