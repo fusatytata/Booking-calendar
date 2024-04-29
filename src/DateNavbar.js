5773877aa6f865532145ab5ca3c82bc4de20ba84
fusatytata <simon.stripes@gmail.com>
2024-04-29 15:29:53 +0200

    DateNavbar controling date added with function reading current date via moment()

---
+++ src/DateNavbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import DateButton from "./DateButton";
+import DecreaseButton from "./DecreaseButton";
+import IncreaseButton from "./IncreaseButton";
+import moment from "moment/moment";
+
+function DateNavbar({date, setDate}) {
+
+const decreaseOne = () => {
+    const newValue = date.clone().subtract(1, "d");
+    setDate(newValue);
+};
+
+const increaseOne = () => {
+    const newValue = date.clone().add(1, "d");
+    setDate(newValue);
+};
+
+const formattedDate = (date) => {
+    const today = moment();
+    const tomorrow = moment().add(1, "d");
+
+    if (date.isSame(today, "day")) {
+        return "Today " + date.format("D.M.");
+    } else if (date.isSame(tomorrow, "day")) {
+        return "Tomorrow " + date.format("D.M.");
+    } else {
+        return date.format("dddd D.M.");
+    }
+};
+    return (
+        <div>
+            <DecreaseButton onDecrease={decreaseOne} disabled={date.isSame(moment(), "day")} />
+            <DateButton date={formattedDate(date)} isActive={true} />
+            <DateButton date={formattedDate(date.clone().add(1, "d"))} onClick={increaseOne} />
+            <IncreaseButton onIncrease={increaseOne} />
+        </div>
+    );
+}
+
+export default DateNavbar;
