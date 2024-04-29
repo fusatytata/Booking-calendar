1ec8ea748a4a5206f4c4e56cce0f50178174dc44
fusatytata <simon.stripes@gmail.com>
2024-04-29 15:31:12 +0200

    DayTimeButton component added with basic css

---
+++ src/DayTimeButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./DayTimeButton.css";
+
+function DayTimeButton({ value, capacity, originalCapacity, onClick, isSelected}) {
+    const backgroundColor = isSelected ? "green" : "transparent";
+    return (
+        <div className="DayTimeButton">
+            <button
+                className="DayTimeButton-Button"
+                onClick={onClick}
+                style={{ backgroundColor }}
+            >
+                <div className="DayTimeButton-Capacity">({capacity} / {originalCapacity})</div> 
+                {value}
+            </button>
+        </div>
+    );
+}
+
+export default DayTimeButton;
