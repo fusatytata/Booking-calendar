37384ce07f2e35c9d43b1daaa279c3964ff53aee
fusatytata <simon.stripes@gmail.com>
2024-04-29 15:28:11 +0200

    more unnecessary files deleted, DateButon component added with basic .css

---
+++ src/DateButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./DateButton.css";
+
+function DateButton({ date, isActive }) {
+    const active = isActive ? "#bada55" : "transparent";
+
+    return (
+        <div className="DateButton">
+            <button style={{ backgroundColor: active }}>
+                {date}
+            </button>
+        </div>
+    );
+}
+
+export default DateButton;
+
+/* TODO: jak funkci z DayTimeButton - zelenání poslat zde nebo třeba do MainNavbar? */
+
+
